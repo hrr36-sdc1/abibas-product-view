4ae876cd9ca8b44ef88475256574444fb702d625
tyler bailey <baile320@umn.edu>
2019-02-27 08:08:36 -0600

    Minor linting, updated package.json scripts for prod and test db seeding

--- database/index.js
+++ database/index.js
@@ -7,26 +7,26 @@ const dbTableName = process.env.NODE_ENV === 'prod' ? 'Products' : 'ProductsTest
 const db = new Sequelize(dbTableName, process.env.DB_USER, process.env.PW, {
   host: 'localhost',
   dialect: 'mysql',
-  },
-);
+});
 
 const Images = db.define('images', {
-  img_id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  links: {type: Sequelize.TEXT }
+  img_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  links: { type: Sequelize.TEXT },
 });
 
 const Shoes = db.define('shoes', {
-  id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   colors: { type: Sequelize.STRING, allowNull: false },
   type: { type: Sequelize.STRING, allowNull: false },
   model: { type: Sequelize.STRING, allowNull: false },
   sizes: { type: Sequelize.STRING, allowNull: false },
   price: { type: Sequelize.INTEGER, allowNull: false },
-  image_ID: { type: Sequelize.INTEGER,
+  image_ID: {
+    type: Sequelize.INTEGER,
     references: {
       model: Images,
-      key: 'img_id'
-    }
+      key: 'img_id',
+    },
   },
   review_count: { type: Sequelize.INTEGER, allowNull: false },
   avg_stars: { type: Sequelize.INTEGER, allowNull: false },
