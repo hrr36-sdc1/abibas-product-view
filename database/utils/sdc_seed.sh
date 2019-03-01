f000a0904b7cc03591bd23e4a26dc515afbdb792
tyler bailey <baile320@umn.edu>
2019-02-28 18:56:06 -0600

    Added time count to bash script

--- database/utils/sdc_seed.sh
+++ database/utils/sdc_seed.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #!/usr/bin/env node
 
+start=`date +%s`
+
 for i in `seq 1 20`;
 do
   ID=$i node ./generate_products.js
@@ -10,8 +12,8 @@ done
 
 for i in `seq 1 20`;
 do
-  psql -U postgres -d products -c "\copy shoes(colors, type, model, sizes, price, image_id, review_count, avg_stars) from program 'head -1000000 ./seeds/file$i.csv' delimiter ',' csv header;"
-  psql -U postgres -d products -c "\copy images(links) from program 'head -1000000 ./seeds/imagefile$i.csv' delimiter ',' csv header;"
+  psql -U postgres -d products -c "\copy shoes(id, colors, type, model, sizes, price, image_id, review_count, avg_stars) from program 'head -1000000 ./seeds/file$i.csv' delimiter ',' csv header;"
+  psql -U postgres -d products -c "\copy images(id, links) from program 'head -1000000 ./seeds/imagefile$i.csv' delimiter ',' csv header;"
   cqlsh -e "USE products; COPY shoes (id, colors, type, model, sizes, price, image_id, review_count, avg_stars) FROM './seeds/file$i.csv' WITH HEADER = TRUE;"
   cqlsh -e "USE products; COPY shoes (id, links) FROM './seeds/imagefile$i.csv' WITH HEADER = TRUE;"
   echo seed $i loaded
@@ -25,3 +27,7 @@ do
 done
 
 rm import*.err*
+
+end=`date +%s`
+
+runtime=$((end-start))
