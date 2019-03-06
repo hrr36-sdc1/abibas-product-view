fe311e0e3c05dda0a201864aba9efcc4fb338629
tyler bailey <baile320@umn.edu>
2019-03-05 20:19:09 -0600

    Remove some unneeded code from React

--- client/src/Components/Product-info/related-products.jsx
+++ client/src/Components/Product-info/related-products.jsx
@@ -1,57 +1,27 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 
-class RelatedProducts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      otherImages: []
-    }
-  }
-
-  // componentDidMount() {
-  //   // if (this.state.otherImages.length === this.props.products.length) {
-  //   //   this.updateImages();
-  //   // }
-  //   // console.log(this.state);
-  //   // this.updateImages();
-  // }
-
-  updateImages() {
-    let temp = [];
-    for (let i = 0; i < this.props.products.length; i++) {
-      temp.push(this.getImages(this.props.products[i].image_ID));
-      if (temp.length === 3) {
-        this.setState({
-          otherImages: temp
-        })
-      }
-    }
-  }
-
-  render() {
-    let colors = this.props.currentProduct ? this.props.currentProduct.colors.split(',') : [];
-    return (
-      <div className="related-container">
-        <h5>Available Colors</h5>
-        <ol>
-          {colors.map((color, i) => {
-            return (
-              <li key={i}>{color} {i < colors.length -1? '/': ''}</li>
-            )
-          })}
-        </ol>
-        <div className="other-colors">
-          {this.props.otherImages.map((image, i) => {
-            return (
-              <Image key={i} onClick={()=> {this.props.infoClick(i); this.props.onRelatedClick(i)}}className="other-thumbnail" src={image[i]} roundedCircle />
-            )
-          })}
-        </div>
+const RelatedProducts = (props) => {
+  const colors = props.currentProduct ? props.currentProduct.colors.split(',') : [];
+  return (
+    <div className="related-container">
+      <h5>Available Colors</h5>
+      <ol>
+        {colors.map((color, i) => (
+          <li key={i}>
+            {color}
+            {' '}
+            {i < colors.length - 1 ? '/' : ''}
+          </li>
+        ))}
+      </ol>
+      <div className="other-colors">
+        {props.otherImages.map((image, i) => (
+          <Image key={i} onClick={() => { props.infoClick(i); props.onRelatedClick(i); }} className="other-thumbnail" src={image[i]} roundedCircle />
+        ))}
       </div>
-    )
-
-  }
-}
+    </div>
+  );
+};
 
 export default RelatedProducts;
